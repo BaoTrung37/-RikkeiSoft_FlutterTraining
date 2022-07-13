a69910fd5d0740fe6039f6370baa02f8a6eb1820
BaoTrung37 <baotrung372k@gmail.com>
2022-07-13 14:04:17 +0700

    init project

---
+++ app_chat/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Chat With Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ app_chat/lib/resources/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._private();
+  static const primaryColor = LinearGradient(colors: [
+    Color(0xFFFFA925),
+    Color(0xFFFF7841),
+  ]);
+
+  static const secondaryColor = Color(0xFF411582);
+
+  static const backgroundColor = Color(0xFFFFFFFF);
+
+  static const backgroundSecondaryColor = Color(0xFFFAFAFA);
+
+  static const itemColor = Color(0xFFF5F5F5);
+}
